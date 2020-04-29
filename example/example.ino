b08152761e5ba0aec78baef4ffbbe097537ff24e
johan <joaramirezra@unal.edu.co>
2020-04-29 12:16:40 -0500

    v0.5

--- example/example.ino
+++ example/example.ino
@@ -1,15 +1,36 @@
-#include <Honeywell_pressure_sensors.h>
 #include <SPI.h>
 
-HPS hps(1);
+// using two incompatible SPI devices, A and B. Incompatible means that they need different SPI_MODE
+const int slaveAPin = 53;
+const int slaveBPin = 21;
+
+// set up the speed, data order and data mode
+SPISettings settingsA(2000000, MSBFIRST, SPI_MODE1);
+SPISettings settingsB(16000000, LSBFIRST, SPI_MODE3);
 
 void setup() {
-    Serial.begin(9600);
-    SPI.begin();
-    SPI.beginTransaction(SPISettings(800000, MSBFIRST, SPI_MODE0));
+  // set the Slave Select Pins as outputs:
+  pinMode (slaveAPin, OUTPUT);
+  pinMode (slaveBPin, OUTPUT);
+  // initialize SPI:
+  SPI.begin();
+  // Inicializacion Serial
+  Serial.begin(9600);
 }
 
+uint8_t stat, val1, val2, result;
+
 void loop() {
-  Serial.println(hps.readPressure());
-  delay(500);
+  // read three bytes from device A
+  SPI.beginTransaction(settingsA);
+  digitalWrite (slaveAPin, LOW);
+  // reading only, so data sent does not matter
+  stat = SPI.transfer(0);
+//  val1 = SPI.transfer(0);
+//  val2 = SPI.transfer(0);
+  digitalWrite (slaveAPin, HIGH);
+  SPI.endTransaction();
+  // if stat is 1 or 2, send val1 or val2 else zero
+  Serial.println(String(stat));
+  delay(5);
 }
